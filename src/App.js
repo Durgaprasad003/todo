e3c4c2684e2c61d5d528c9645cb629bd59cc3e00
prasad <Durgaprasad003@gmail.com>
2024-06-16 11:35:22 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./App.css"
+export default function App() {
+       const[todo,input]=useState("test")
+        let[todolist,updatetodo]=useState([
+          {
+            id:1,
+            task:"learn react"
+          },
+          {
+            id:2,
+            task:"learn angular"
+          }
+        ])
+        function show(){
+          let netid=3
+           if(todo==="")
+           {
+            alert("Enter you task")
+           } 
+           else{
+             let newtodo=[...todolist,
+             {
+             id:netid++,
+              task:todo
+             }];
+              updatetodo(newtodo)
+               input("")
+           }
+        }
 
-function App() {
+        function deletetodo(id){
+        let t=  todolist.filter((todo)=>{
+                   return todo.id!=id
+          })
+    
+          updatetodo(t)
+
+        }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mt-5 w-50'>
+    <h3 className='text-center'>Todo</h3>
+            <div className='input-group'>
+                    <input type="text" className='form-control' value={todo} onChange={(e)=>{
+                     
+                        input(e.target.value)
+                        // console.log(todo)
+                        // console.log(e.target.value)
+                    }} />
+                    <button className='btn btn-primary' onClick={show}>Add</button>           
+            </div>
+            <ul className='list-group mt-4'>
+              {
+                todolist.map((todo)=>{
+                  return(
+                    <li className='list-group-item'key={todo.id}><p >{todo.task}</p> <button onClick={()=>{deletetodo(todo.id)}} className='btn'>❌</button></li>
+                  )
+                })
+              }
+            </ul>
     </div>
-  );
+  )
 }
-
-export default App;
